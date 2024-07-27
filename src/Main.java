61994a58f82bed5d6548797acad47c9643b40948
TurdaliyevKayratbek <buumer2006@gmail.com>
2024-07-27 13:31:23 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,185 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        1)System.out.println("Vvodi:");
+//        Scanner scanner=new Scanner(System.in);
+//        int now=scanner.nextInt();
+//        int q=now%10;
+//        int w=now/10%10;
+//        int e=now/100;
+//        int r=((q*100)+(w*10)+(e))-20;
+//        System.out.println("Chisla: "+r);
+
+
+//        2)System.out.println("Input: ");
+//        int sc=new Scanner(System.in).nextInt();
+//
+//        System.out.println(((sc) >= 0) ? sc*2+" Krasavchik" : "Loh");
+
+
+
+//        3)System.out.println("Input:");
+//        int sc=new Scanner(System.in).nextInt();
+//
+//        if (sc>0) System.out.println("Positive");
+//        else if (sc<0) System.out.println("Negative");
+//        else System.out.println("Zero");
+
+
+
+//        4)System.out.println("Input: ");
+//        int number1=new Scanner(System.in).nextInt();
+//        System.out.println((number1>99)? +number1-1+" Unfortunately minus 1":"FLCL");
+//
+//        5)System.out.println("Amount of students: ");
+//        int students=new Scanner(System.in).nextInt();
+//        System.out.println("Amount of chairs: ");
+//        int chairs=new Scanner(System.in).nextInt();
+//        if (students==chairs) System.out.println("Enough amount of chairs for students");
+//        else if (students>chairs) System.out.println("Not enough chairs");
+//        else System.out.println("There are few students");
+
+
+//        7)System.out.println("Input: ");
+//        int q=new Scanner(System.in).nextInt();
+//        if (q>0) {
+//            double w=Math.pow(q, 2);
+//            System.out.println("Kvadrat: "+w);
+//        }
+//        else System.out.println("Oshibka");;
+
+//        8)System.out.println("Input: ");
+//        int scanner=new Scanner(System.in).nextInt();
+//        int law1=scanner%10;
+//        if (law1>5) {
+//            int trf=10-law1;
+//            int trf1=scanner+trf;
+//            System.out.println("Molodchik: "+trf1);
+//        }
+//        else System.out.println(scanner-law1);
+
+
+//        9)String month=new Scanner(System.in).nextLine();
+//
+//        switch (month) {
+//            case "1":
+//                System.out.println("Jan:Winter");
+//                break;
+//            case "2":
+//                System.out.println("Feb:Winter");
+//                break;
+//            case "3":
+//                System.out.println("March:Spring");
+//                break;
+//            case "4":
+//                System.out.println("April:Spring");
+//                break;
+//            case "5":
+//                System.out.println("May:Spring");
+//                break;
+//            case "6":
+//                System.out.println("June:Summer");
+//                break;
+//            case "7":
+//                System.out.println("July:Summer");
+//                break;
+//            case "8":
+//                System.out.println("Aug:Summer");
+//                break;
+//            case "9":
+//                System.out.println("Sep:Fall");
+//                break;
+//            case "10":
+//                System.out.println("Oct:Fall");
+//                break;
+//            case "11":
+//                System.out.println("Nov:Fall");
+//                break;
+//            case "12":
+//                System.out.println("Dec:Winter");
+//                break;
+//            default:
+//                System.out.println("Wrong one huh?");
+
+
+//        10)System.out.println("input: ");
+//        int num=new Scanner(System.in).nextInt();
+//        if (num <100 || num >1000) {
+//            System.out.println("Wrong. Try again");
+//        } else {
+//            int q = num / 100;
+//            int w= (num / 10) % 10;
+//            int e = num % 10;
+//
+//            System.out.println("The things has lotta nums:");
+
+
+
+
+
+
+//
+//        11)Scanner scanner = new Scanner(System.in);
+//        System.out.print("Input: ");
+//        char q = scanner.next().charAt(0);
+//
+//        if ((q > 'A' && q < 'Z') || (q > 'a' && q < 'z')) System.out.printf("Symbol is latin", q);
+//        else System.out.printf("Symbol ain't no latin", q);
+
+
+
+//        12)System.out.println("Input 1st: ");
+//        String name=new Scanner(System.in).nextLine();
+//        System.out.println("Input 2st: ");
+//        String name2=new Scanner(System.in).nextLine();
+//        System.out.println((name==name2)? "Netezka":"Tezka");
+//
+//        System.out.println("Input some: ");
+//        String language=new Scanner(System.in).nextLine();
+//        String language2=new Scanner(System.in).nextLine();
+//        System.out.println((language));
+
+
+
+//
+//        13)System.out.println("RPC");
+//        int hd=new Scanner(System.in).nextInt();
+//        Random random=new Random();
+//        int hq=random.nextInt(1, 3);
+//
+//        if (hd==1) System.out.println("Rock");
+//        else if (hd==2) System.out.println("Paper");
+//        else if (hd==3) System.out.println("Scissors");
+//        else System.out.println("Don't throw any gang signs here mate!");
+//
+//        if (hq==1) System.out.println("Rock");
+//        else if (hq==2) System.out.println("Paper");
+//        else if (hq==3) System.out.println("Scissors");
+//        else System.out.println("No more questions!");
+//
+//        if (hd==hq) System.out.println("Draw");
+//        else if (hd==1 && hq==3) System.out.println("WINNER");
+//        else if (hd==2 && hq==1) System.out.println("WINNER");
+//        else if (hd==3 && hq==2) System.out.println("WINNER");
+//        else System.out.println("Ulost");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
